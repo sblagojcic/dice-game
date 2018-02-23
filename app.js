374ea8a9e7c09529cae46b0e6de47f5464ee222f
Stevan Blagojčić <sblagojcic@gmail.com>
2018-02-23 14:19:33 +0100

    dice game final

---
+++ app.js
@@ -0,0 +1,106 @@
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying){
+
+        //1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+        //3. Update the round score IF rolled number was NOT a 1
+        if(dice !== 1){
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }else{
+           //next player
+            nextPlayer();
+        }
+    }
+    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    if(gamePlaying){
+        //1. Adding CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+
+        //2. Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //3. Check if player won the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-'+ activePlayer).textContent = 'Pobednik !';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }else{
+            //4. Next player
+            nextPlayer();
+
+    }
+
+    }
+   
+});
+
+function nextPlayer(){
+     //next player
+       // activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        if(activePlayer === 0){
+            activePlayer = 1; 
+        }else{
+            activePlayer = 0;
+        }
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Igrac 1';
+    document.getElementById('name-1').textContent = 'Igrac 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
+
+
+
+
+
+
+
+
+
